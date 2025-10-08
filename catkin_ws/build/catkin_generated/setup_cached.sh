4e32cfa1f63d052916dfe79ce51a7ab14f512608
张桓瑜 <2956226494@qq.com>
2025-10-08 06:06:17 -0700

    first commit: 初始化项目，添加核心文件

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/zhanghunayu/DRL-robot-navigation/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/mnt/newdisk/mydata/anaconda3/envs/pytorch/bin:/mnt/newdisk/mydata/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/zhanghuanyu/anaconda3/bin'
+export PWD='/home/zhanghunayu/DRL-robot-navigation/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/zhanghunayu/DRL-robot-navigation/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/zhanghunayu/DRL-robot-navigation/catkin_ws/src:$ROS_PACKAGE_PATH"
